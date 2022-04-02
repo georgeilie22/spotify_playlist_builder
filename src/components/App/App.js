21762f7070a2ac9c6d1e124e98d34d94fc4b62ef
George Ilie <georgedavidilie@gmail.com>
2022-04-02 16:24:11 +0300

    added search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,37 +3,15 @@ import "./App.css";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
+import Spotify from "../../utils/Spotify";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [
-        { name: "name1", artist: "artist1", album: "album1", id: 1 },
-        { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
-      ],
+      searchResults: [],
       playlistName: "My playlist",
-      playlistTracks: [
-        {
-          name: "playlistName1",
-          artist: "playlistArtist1",
-          album: "playlisAlbum1",
-          id: 4,
-        },
-        {
-          name: "playlistName2",
-          artist: "playlistArtist2",
-          album: "playlisAlbum2",
-          id: 5,
-        },
-        {
-          name: "playlistName3",
-          artist: "playlistArtist3",
-          album: "playlisAlbum3",
-          id: 6,
-        },
-      ],
+      playlistTracks: [],
     };
 
     this.addTrack = this.addTrack.bind(this);
@@ -73,7 +51,9 @@ class App extends React.Component {
   }
 
   search(searchValue) {
-    console.log(searchValue);
+    Spotify.search(searchValue).then((searchResult) => {
+      this.setState({ searchResults: searchResult });
+    });
   }
 
   render() {
