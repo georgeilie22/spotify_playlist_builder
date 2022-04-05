01c3720ff1c23ddbc66ea2c3b475d3c80d35e961
George Ilie <georgedavidilie@gmail.com>
2022-04-05 21:04:12 +0300

    functional components finished

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -31,41 +31,4 @@ const Playlist = (props) => {
   );
 };
 
-// class Playlist extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleNameChange = this.handleNameChange.bind(this);
-//     this.emptyPlaylistName = this.emptyPlaylistName.bind(this);
-//   }
-
-//   handleNameChange(event) {
-//     this.props.onNameChange(event.target.value);
-//   }
-
-//   emptyPlaylistName() {
-//     this.props.onNameChange("");
-//   }
-
-//   render() {
-//     return (
-//       <div className="Playlist">
-//         <input
-//           value={this.props.playlistName}
-//           onFocus={this.emptyPlaylistName}
-//           onChange={this.handleNameChange}
-//         />
-//         <TrackList
-//           tracks={this.props.playlistTracks}
-//           onAdd={this.props.onAdd}
-//           onRemove={this.props.onRemove}
-//           isRemoval={true}
-//         />
-//         <button className="Playlist-save" onClick={this.props.onSave}>
-//           SAVE TO SPOTIFY
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
 export default Playlist;
